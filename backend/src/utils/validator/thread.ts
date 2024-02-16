67bc0487ebb3eb2b7da69a9e26cf1415b7a4f4f6
alfanrckz <alfansyuriziaulhaq11@gmail.com>
2024-02-16 14:41:12 +0700

    update-done

--- backend/src/utils/validator/thread.ts
+++ backend/src/utils/validator/thread.ts
@@ -1,12 +1,12 @@
 import joi = require("joi");
 
 export const createThreadSchema = joi.object({
-  content: joi.string().required(),
-  image: joi.string().optional(),
+  content: joi.string().allow(null),
+  image: joi.string().allow(null).optional(),
   user: joi.number().required(),
 });
 export const updateThreadSchema = joi.object({
-  content: joi.string().required(),
+  content: joi.string().optional(),
   image: joi.string().optional(),
-  updated_at: joi.date().default(Date.now()),
+  updated_at: joi.date().default(new Date()),
 });
