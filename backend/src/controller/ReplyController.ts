f8f90f8d7ff0b6d68c0b9f5f8f33b65d1f8821d8
alfanrckz <alfansyuriziaulhaq11@gmail.com>
2024-02-20 11:30:44 +0700

    reply-controller

--- backend/src/controller/ReplyController.ts
+++ backend/src/controller/ReplyController.ts
@@ -5,7 +5,32 @@ export default new (class ReplyController {
   find(req: Request, res: Response) {
     ReplyServices.find(req, res);
   }
-  create(req: Request, res: Response) {
-    ReplyServices.create(req, res);
+  // create(req: Request, res: Response) {
+  //   ReplyServices.create(req, res);
+  // }
+
+  async create(req: Request, res: Response) {
+    try {
+      let data;
+      if (!req.file) {
+        data = {
+          content: req.body.content,
+          user: req.body.user,
+          thread: req.body.thread,
+        };
+      } else {
+        data = {
+          content: req.body.content,
+          image: req.file.filename,
+          user: req.body.user,
+          thread: req.body.thread,
+        };
+      }
+
+      const response = await ReplyServices.create(data);
+      res.status(201).json(response);
+    } catch (error) {
+      res.status(error.status).json(error.message);
+    }
   }
 })();
