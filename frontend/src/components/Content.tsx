38361fccfd81fb223996ea1f34ab67ad2d2e3048
alfanrckz <alfansyuriziaulhaq11@gmail.com>
2024-02-26 23:42:35 +0700

    menu-bar

--- frontend/src/components/Content.tsx
+++ frontend/src/components/Content.tsx
@@ -1,117 +1,117 @@
-import {
-  Box,
-  Image,
-  Card,
-  CardBody,
-  Flex,
-  Input,
-  Center,
-  Button,
-  Icon,
-  FormControl,
-} from "@chakra-ui/react";
+// import {
+//   Box,
+//   Image,
+//   Card,
+//   CardBody,
+//   Flex,
+//   Input,
+//   Center,
+//   Button,
+//   Icon,
+//   FormControl,
+// } from "@chakra-ui/react";
 
-import { BiSolidImageAdd } from "react-icons/bi";
-import ListContents from "./ListContent";
-import { useSelector } from "react-redux";
-import { RootState } from "../stores/types/rootState";
-import { useThreads } from "../features/thread/hooks/useThreads";
-// import { any } from "joi";
+// import { BiSolidImageAdd } from "react-icons/bi";
+// import ListContents from "./ListContent";
+// import { useSelector } from "react-redux";
+// import { RootState } from "../stores/types/rootState";
+// import { useThreads } from "../features/thread/hooks/useThreads";
+// // import { any } from "joi";
 
-export default function Content() {
-  const auth = useSelector((state: RootState) => state.auth);
-  const { handleChange, handlePost, fileInputRef, handleButtonClick, threads } =
-    useThreads();
-  // console.log(handleButtonClick);
-  // console.log("ini threadss", threads);
-  // console.log("ini handlepost", handlePost);
+// export default function Content() {
+//   const auth = useSelector((state: RootState) => state.auth);
+//   const { handleChange, handlePost, fileInputRef, handleButtonClick, threads } =
+//     useThreads();
+//   // console.log(handleButtonClick);
+//   // console.log("ini threadss", threads);
+//   // console.log("ini handlepost", handlePost);
 
-  // const sortedThreads = threads
-  //   ?.slice()
-  //   .sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+//   // const sortedThreads = threads
+//   //   ?.slice()
+//   //   .sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
 
-  return (
-    <>
-      <Box m={4}>
-        <Card
-          direction={{ base: "column", sm: "row" }}
-          overflow="hidden"
-          variant="outline"
-          bg="mainBg.200"
-          borderColor="main.Bg.200"
-          border="1px"
-        >
-          <Image
-            borderRadius="100%"
-            objectFit="cover"
-            h={14}
-            w={14}
-            marginLeft={4}
-            marginTop={4}
-            maxW={{ base: "100%", sm: "200px" }}
-            src={auth.picture ? auth.picture : "/placeholder-profile.jpg"}
-            alt="Caffe Latte"
-          />
-          <CardBody>
-            <form onSubmit={handleButtonClick} encType="multipart/form-data">
-              <Flex color="white">
-                <Input
-                  placeholder="What's Happening?!"
-                  size="lg"
-                  marginX="auto"
-                  border="none"
-                  name="content"
-                  onChange={handleChange}
-                />
-                <Center>
-                  <label style={{ cursor: "pointer" }}>
-                    <Input
-                      name="image"
-                      type="file"
-                      hidden
-                      onChange={handleChange}
-                      ref={fileInputRef}
-                    />
-                    <Icon
-                      mr={"10px"}
-                      ml={"10px"}
-                      color={"green"}
-                      boxSize={8}
-                      as={BiSolidImageAdd}
-                    />
-                  </label>
-                </Center>
+//   return (
+//     <>
+//       <Box m={4}>
+//         <Card
+//           direction={{ base: "column", sm: "row" }}
+//           overflow="hidden"
+//           variant="outline"
+//           bg="mainBg.200"
+//           borderColor="main.Bg.200"
+//           border="1px"
+//         >
+//           <Image
+//             borderRadius="100%"
+//             objectFit="cover"
+//             h={14}
+//             w={14}
+//             marginLeft={4}
+//             marginTop={4}
+//             maxW={{ base: "100%", sm: "200px" }}
+//             src={auth.picture ? auth.picture : "/placeholder-profile.jpg"}
+//             alt="Caffe Latte"
+//           />
+//           <CardBody>
+//             <form onSubmit={handleButtonClick} encType="multipart/form-data">
+//               <Flex color="white">
+//                 <Input
+//                   placeholder="What's Happening?!"
+//                   size="lg"
+//                   marginX="auto"
+//                   border="none"
+//                   name="content"
+//                   onChange={handleChange}
+//                 />
+//                 <Center>
+//                   <label style={{ cursor: "pointer" }}>
+//                     <Input
+//                       name="image"
+//                       type="file"
+//                       hidden
+//                       onChange={handleChange}
+//                       ref={fileInputRef}
+//                     />
+//                     <Icon
+//                       mr={"10px"}
+//                       ml={"10px"}
+//                       color={"green"}
+//                       boxSize={8}
+//                       as={BiSolidImageAdd}
+//                     />
+//                   </label>
+//                 </Center>
 
-                <Button
-                  colorScheme="green"
-                  ml={3}
-                  borderRadius="20%"
-                  value={"Post"}
-                  onClick={handlePost}
-                >
-                  submit
-                </Button>
-              </Flex>
-            </form>
-          </CardBody>
-        </Card>
-      </Box>
-      {/* content */}
-      {threads?.map((item) => {
-        return (
-          <ListContents
-            key={item.id}
-            id={item.id}
-            user={item?.user}
-            content={item.content}
-            created_at={item.created_at}
-            image={item.image}
-            count_like={item.count_like}
-            count_replies={item.count_replies}
-            is_liked={item.is_liked}
-          />
-        );
-      })}
-    </>
-  );
-}
+//                 <Button
+//                   colorScheme="green"
+//                   ml={3}
+//                   borderRadius="20%"
+//                   value={"Post"}
+//                   onClick={handlePost}
+//                 >
+//                   submit
+//                 </Button>
+//               </Flex>
+//             </form>
+//           </CardBody>
+//         </Card>
+//       </Box>
+//       {/* content */}
+//       {threads?.map((item) => {
+//         return (
+//           <ListContents
+//             key={item.id}
+//             id={item.id}
+//             user={item?.user}
+//             content={item.content}
+//             created_at={item.created_at}
+//             image={item.image}
+//             count_like={item.count_like}
+//             count_replies={item.count_replies}
+//             is_liked={item.is_liked}
+//           />
+//         );
+//       })}
+//     </>
+//   );
+// }
--- frontend/src/components/MenuBar.tsx
+++ frontend/src/components/MenuBar.tsx
@@ -1,124 +1,137 @@
-// import {
-//   Box,
-//   Heading,
-//   Button,
-//   ListItem,
-//   UnorderedList,
-//   Text,
-//   Flex,
-//   Center,
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  Heading,
+  ListItem,
+  Spacer,
+  Text,
+  UnorderedList,
+} from "@chakra-ui/react";
 
-//   // ExternalLinkIcon,
-// } from "@chakra-ui/react";
+import { CgProfile } from "react-icons/cg";
+import { CiLogout } from "react-icons/ci";
+import { GoHeartFill } from "react-icons/go";
+import { IoMdHome } from "react-icons/io";
+import { MdOutlinePersonSearch } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { AUTH_LOGOUT } from "../stores/rootReducer";
+import { useDispatch } from "react-redux";
 
-// import { CgProfile } from "react-icons/cg";
-// import { IoMdHome } from "react-icons/io";
-// import { MdOutlinePersonSearch } from "react-icons/md";
-// import { GoHeartFill } from "react-icons/go";
-// import { CiLogout } from "react-icons/ci";
-// import { Link } from "react-router-dom";
-// import { useDispatch } from "react-redux";
-// import { AUTH_LOGOUT } from "../stores/rootReducer";
-
-// export default function MenuBar() {
-//   const dispatch = useDispatch();
-//   const handleLogout = () => {
-//     localStorage.removeItem("authData");
-//     dispatch(AUTH_LOGOUT());
-//     // localStorage.removeItem("token");
-//     // localStorage.removeItem("id");
-//     // document.cookie = "C.id=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
-//     window.location.href = "/login";
-//   };
-
-//   return (
-//     <>
-//       <Box pos="fixed">
-//         <Box m={4}>
-//           <Heading color="green">Circle</Heading>
-//           <UnorderedList my={4} style={{ listStyleType: "none" }}>
-//             <ListItem my={2}>
-//               <Flex>
-//                 <Center>
-//                   <IoMdHome />
-//                   <Link to={"/"}>
-//                     <Text
-//                       bg="mainBg.900"
-//                       pl={2}
-//                       _hover={{ textDecoration: "none", fontWeight: "bold" }}
-//                     >
-//                       HOME
-//                     </Text>
-//                   </Link>
-//                 </Center>
-//               </Flex>
-//             </ListItem>
-//             <ListItem my={2}>
-//               <Flex>
-//                 <Link to="/search">
-//                   <Center>
-//                     <MdOutlinePersonSearch />
-//                     <Text
-//                       bg="mainBg.900"
-//                       pl={2}
-//                       _hover={{ textDecoration: "none", fontWeight: "bold" }}
-//                     >
-//                       SEARCH
-//                     </Text>
-//                   </Center>
-//                 </Link>
-//               </Flex>
-//             </ListItem>
-//             <ListItem my={2}>
-//               <Flex>
-//                 <Center>
-//                   <GoHeartFill />
-//                   <Link to="/follow">
-//                     <Text
-//                       bg="mainBg.900"
-//                       pl={2}
-//                       _hover={{ textDecoration: "none", fontWeight: "bold" }}
-//                     >
-//                       FOLLOWS
-//                     </Text>
-//                   </Link>
-//                 </Center>
-//               </Flex>
-//             </ListItem>
-//             <ListItem my={2}>
-//               <Flex>
-//                 <Center>
-//                   <CgProfile />
-//                   <Link to="/detail-profile">
-//                     <Text
-//                       bg="mainBg.900"
-//                       pl={2}
-//                       _hover={{ textDecoration: "none", fontWeight: "bold" }}
-//                     >
-//                       PROFILE
-//                     </Text>
-//                   </Link>
-//                 </Center>
-//               </Flex>
-//             </ListItem>
-//           </UnorderedList>
-//         </Box>
-//         <Center>
-//           <Button
-//             w={"100%"}
-//             mx={4}
-//             color={"white"}
-//             bg={"green"}
-//             variant={"solid"}
-//           >
-//             Create Post
-//           </Button>
-//         </Center>
-//         <Center mt="290" onClick={handleLogout}>
-//           <CiLogout />
-//           <Text cursor="pointer">Logout</Text>
-//         </Center>
-//       </Box>
-//     </>
-//   );
-// }
+export default function MenuBar() {
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    localStorage.removeItem("authData");
+    dispatch(AUTH_LOGOUT());
+    // localStorage.removeItem("token");
+    // localStorage.removeItem("id");
+    // document.cookie = "C.id=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    window.location.href = "/login";
+  };
+  return (
+    <Box pos="fixed">
+      <Box m={4}>
+        <Heading color="green">Circle</Heading>
+        <UnorderedList my={4} style={{ listStyleType: "none" }}>
+          <ListItem my={2}>
+            <Flex>
+              <Center>
+                <IoMdHome />
+                <Link to={"/"}>
+                  <Text
+                    bg="mainBg.900"
+                    pl={2}
+                    _hover={{
+                      textDecoration: "none",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    HOME
+                  </Text>
+                </Link>
+              </Center>
+            </Flex>
+          </ListItem>
+          <ListItem my={2}>
+            <Flex>
+              <Center>
+                <MdOutlinePersonSearch />
+                <Link to={"/search"}>
+                  <Text
+                    bg="mainBg.900"
+                    pl={2}
+                    _hover={{
+                      textDecoration: "none",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    SEARCH
+                  </Text>
+                </Link>
+              </Center>
+            </Flex>
+          </ListItem>
+          <ListItem my={2}>
+            <Flex>
+              <Center>
+                <GoHeartFill />
+                <Link to={"/follow"}>
+                  <Text
+                    bg="mainBg.900"
+                    pl={2}
+                    _hover={{
+                      textDecoration: "none",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    FOLLOWS
+                  </Text>
+                </Link>
+              </Center>
+            </Flex>
+          </ListItem>
+          <ListItem my={2}>
+            <Flex>
+              <Center>
+                <CgProfile />
+                <Link to={"/detail-profile"}>
+                  <Text
+                    bg="mainBg.900"
+                    pl={2}
+                    _hover={{
+                      textDecoration: "none",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    PROFILE
+                  </Text>
+                </Link>
+              </Center>
+            </Flex>
+          </ListItem>
+        </UnorderedList>
+      </Box>
+      <Center>
+        <Button
+          w={"100%"}
+          mx={4}
+          color={"white"}
+          bg={"green"}
+          variant={"solid"}
+          _hover={{
+            textDecoration: "none",
+            fontWeight: "bold",
+          }}
+        >
+          Create Post
+        </Button>
+      </Center>
+      <Spacer />
+      <Center mt={310} onClick={handleLogout}>
+        <CiLogout />
+        <Text cursor="pointer">Logout</Text>
+      </Center>
+    </Box>
+  );
+}
