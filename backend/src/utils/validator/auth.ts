0066fbcee8911b23a4100a29db9b4370064ab133
alfanrckz <alfansyuriziaulhaq11@gmail.com>
2024-02-28 17:28:13 +0700

    auth-validator

--- backend/src/utils/validator/auth.ts
+++ backend/src/utils/validator/auth.ts
@@ -2,7 +2,7 @@ import Joi = require("joi");
 
 export const registerSchema = Joi.object().keys({
   fullName: Joi.string().required(),
-  username: Joi.string().required().min(5).max(10),
+  username: Joi.string().required().min(5),
   email: Joi.string().email().required().min(5).max(50),
   password: Joi.string().required().min(5).max(50),
 });
