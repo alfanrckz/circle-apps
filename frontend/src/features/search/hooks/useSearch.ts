433a4941346e7d9a8da2245e652292b6f12540e2
alfanrckz <alfansyuriziaulhaq11@gmail.com>
2024-02-28 22:10:23 +0700

    use-search

---
+++ frontend/src/features/search/hooks/useSearch.ts
@@ -0,0 +1,70 @@
+// import { useDispatch, useSelector } from "react-redux";
+// import { RootState } from "../../../stores/types/rootState";
+// import { API } from "../../../libs/api";
+// import { GET_USERS } from "../../../stores/rootReducer";
+// import { useState } from "react";
+
+// export function useSearch() {
+//   const dispatch = useDispatch();
+//   const users = useSelector((state: RootState) => state.user.user);
+//   const [filteredUsers, setFilteredUsers] = useState(users);
+
+//   async function getUsers() {
+//     try {
+//       const response = await API.get("/users");
+//       dispatch(GET_USERS(response.data));
+//       console.log(response.data);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//     getUsers();
+//   }
+
+//   const searchUsers = (searchQuery: string) => {
+//     const filtered = users.filter((user) =>
+//       user.fullName?.toLowerCase().includes(searchQuery.toLowerCase())
+//     );
+//     setFilteredUsers(filtered);
+//   };
+
+//   return { filteredUsers, getUsers, searchUsers, users };
+// }
+
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../stores/types/rootState";
+import { API } from "../../../libs/api";
+import { GET_USERS } from "../../../stores/rootReducer";
+import { useEffect, useState } from "react"; // Perlu mengimpor useEffect
+
+export function useSearch() {
+  const dispatch = useDispatch();
+  const users = useSelector((state: RootState) => state.user.user);
+  console.log(users);
+
+  const [filteredUsers, setFilteredUsers] = useState(users);
+
+  useEffect(() => {
+    // Gunakan useEffect untuk memuat data pengguna saat komponen dimuat
+    async function fetchData() {
+      try {
+        const response = await API.get("/users");
+        dispatch(GET_USERS(response.data));
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {}, []);
+
+  const searchUsers = (searchQuery: string) => {
+    const filtered = users.filter((user) =>
+      user.fullName?.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredUsers(filtered);
+  };
+
+  return { filteredUsers, searchUsers, users };
+}
