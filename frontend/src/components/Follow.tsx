122164c4d2648d5344472ce8be2842dc213eccc7
alfanrckz <alfansyuriziaulhaq11@gmail.com>
2024-03-01 23:27:35 +0700

    follow

--- frontend/src/components/Follow.tsx
+++ frontend/src/components/Follow.tsx
@@ -1,34 +1,42 @@
 import {
   Box,
-  Flex,
-  Heading,
+  Card,
   Tab,
   TabList,
   TabPanel,
   TabPanels,
   Tabs,
-  Text,
 } from "@chakra-ui/react";
 
 export default function Follow() {
   return (
-    <>
-      <Tabs
-        borderColor={"black"}
-        isFitted
-        width={"100%"}
-        marginTop={"20px"}
-        variant="enclosed"
-      >
-        <TabList mb={"1em"} borderColor={"black"}>
-          <Tab>Followers</Tab>
-          <Tab>Followings</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel></TabPanel>
-          <TabPanel></TabPanel>
-        </TabPanels>
-      </Tabs>
-    </>
+    <Box h={"97vh"} color={"white"} mt={4}>
+      <Card mx={4} mb={2} p="5px" h="100%" bg={"mainBg.200"} color={"white"}>
+        <Tabs
+          px={5}
+          borderColor={"black"}
+          isFitted
+          width={"100%"}
+          marginTop={"20px"}
+          variant="enclosed"
+        >
+          <TabList mb={"1em"} borderColor={"black"}>
+            <Tab>Followers</Tab>
+            <Tab>Followings</Tab>
+          </TabList>
+          <TabPanels
+            h={"60vh"}
+            overflowY={"auto"}
+            sx={{
+              "&::-webkit-scrollbar": { width: "5px", borderRadius: "full" },
+              "&::-webkit-scrollbar-thumb": { bg: "green.500" },
+            }}
+          >
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Card>
+    </Box>
   );
 }
