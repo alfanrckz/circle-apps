e8af7a2bd2882577da64ced2505cdcc282a739b2
alfanrckz <alfansyuriziaulhaq11@gmail.com>
2024-03-05 19:50:07 +0700

    profile-detail

--- frontend/src/components/ProfileDetailComp.tsx
+++ frontend/src/components/ProfileDetailComp.tsx
@@ -38,18 +38,18 @@ const ProfileDetailComp: React.FC = () => {
     <>
       <Box
         color={"white"}
-        mt={4}
-        h={"97vh"}
+        m={4}
+        h={"100vh"}
         overflowY={"auto"}
         sx={{
           "&::-webkit-scrollbar": { width: "5px", borderRadius: "full" },
           "&::-webkit-scrollbar-thumb": { bg: "green.500" },
         }}
       >
+        <Text ml={2} fontWeight={"bold"} fontSize={"2xl"} my={2}>
+          Profile
+        </Text>
         <Card mx={4} mb={2} p="5px" h="330px" bg={"mainBg.200"} color={"white"}>
-          <Text fontWeight="500" my={2} mx={4}>
-            Profile
-          </Text>
           <Flex direction="column" alignItems="center" mx={2}>
             <Image
               src="https://png.pngtree.com/background/20220724/original/pngtree-ackground-hijau-keren-dan-kosong-abstract-untuk-wallpaper-template-desain-ppt-picture-image_1741397.jpg"
@@ -99,13 +99,24 @@ const ProfileDetailComp: React.FC = () => {
                 fontWeight="700"
                 textTransform={"capitalize"}
               >
-                {auth.fullName}
+                ✨{auth.fullName}✨
               </Text>
-              <Text textAlign={"left"} fontSize="10px" fontWeight={"500"}>
+              <Text
+                color={"gray.400"}
+                textAlign={"left"}
+                fontSize="12px"
+                fontWeight={"500"}
+              >
                 @{auth.username}
               </Text>
-              <Text textAlign={"left"} fontSize="12px" fontWeight="600">
-                Siksa kubur berat broooo
+              <Text
+                color={"gray.300"}
+                textAlign={"left"}
+                fontSize="15px"
+                fontWeight="600"
+              >
+                Siksa kubur berat broooo maka perbuatlah kebaikan di dunia
+                walaupun engkau sedang di banned👍
                 {auth.bio}
               </Text>
             </Box>
@@ -123,8 +134,8 @@ const ProfileDetailComp: React.FC = () => {
                 content={item.content}
                 created_at={item.created_at}
                 image={item.image}
-                count_like={item.count_like}
-                count_replies={item.count_replies}
+                likes={item.likes}
+                replies={item.replies}
                 is_liked={item.is_liked}
               />
             );
