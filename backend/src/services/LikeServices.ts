2e835ccf4025bc00c0f455a5109292707fd85b53
alfanrckz <alfansyuriziaulhaq11@gmail.com>
2024-03-13 20:47:46 +0700

    like-services

--- backend/src/services/LikeServices.ts
+++ backend/src/services/LikeServices.ts
@@ -3,6 +3,7 @@ import { Like } from "../entity/Like";
 import { AppDataSource } from "../data-source";
 import { Request, Response } from "express";
 import { createLikeSchema } from "../utils/validator/like";
+import { threadId } from "worker_threads";
 
 export default new (class LikeServices {
   private readonly likeRepository: Repository<Like> =
@@ -46,7 +47,7 @@ export default new (class LikeServices {
         },
       });
 
-      //unlike handle
+      // unlike handle
       if (likeSelected) {
         await this.likeRepository.remove(likeSelected);
         return res.status(200).json({
@@ -75,4 +76,71 @@ export default new (class LikeServices {
       });
     }
   }
+
+  // async create(reqBody: any, loginSession: any): Promise<any> {
+  //   try {
+  //     const isLikeExist = await this.likeRepository.count({
+  //       where: {
+  //         user: {
+  //           id: loginSession.user.id,
+  //         },
+  //         thread: {
+  //           id: reqBody.thread_id,
+  //         },
+  //       },
+  //     });
+
+  //     if (isLikeExist > 0) {
+  //       throw new Error("You already like this thread!");
+  //     }
+
+  //     const like = this.likeRepository.create({
+  //       thread: {
+  //         id: reqBody.thread_id,
+  //       },
+  //       user: {
+  //         id: loginSession.user.id,
+  //       },
+  //     });
+
+  //     await this.likeRepository.save(like);
+
+  //     return {
+  //       message: "You liked this thread!",
+  //       like: like,
+  //     };
+  //   } catch (error) {
+  //     throw new Error(error.message);
+  //   }
+  // }
+
+  // async delete(threadId: number, loginSession: any): Promise<any> {
+  //   try {
+  //     const like = await this.likeRepository.findOne({
+  //       where: {
+  //         thread: {
+  //           id: threadId,
+  //         },
+  //         user: {
+  //           id: loginSession.user.id,
+  //         },
+  //       },
+  //     });
+
+  //     if (!like) {
+  //       throw new Error("You didn't like this thread!");
+  //     }
+
+  //     await this.likeRepository.delete({
+  //       id: like.id,
+  //     });
+
+  //     return {
+  //       message: "You unliked this thread!",
+  //       like: like,
+  //     };
+  //   } catch (error) {
+  //     throw new Error(error.message);
+  //   }
+  // }
 })();
