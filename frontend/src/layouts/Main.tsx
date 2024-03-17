848465ba4da86e2bcb5ffadbb072f7c5a9a7679f
alfanrckz <alfansyuriziaulhaq11@gmail.com>
2024-03-17 22:01:38 +0700

    layouts

--- frontend/src/layouts/Main.tsx
+++ frontend/src/layouts/Main.tsx
@@ -1,27 +1,14 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import { ReactNode } from "react";
-
 import MyProfile from "../components/MyProfile";
 import MenuBar from "../components/MenuBar";
 
 export default function Main({ children }: { children: ReactNode }) {
-  // const navigate = useNavigate();
-  // const location = useLocation();
-  // const auth = useSelector((state: RootState) => state.auth);
-  // console.log(auth);
-
-  // useEffect(() => {
-  //   const storeAuthData = localStorage.getItem("authData");
-  //   if (storeAuthData) {
-  //     const parsedAuthData = JSON.parse(storeAuthData);
-  //     dispatch(AUTH_CHECK({ parsedAuthData }));
-  //   }
-  // }, [dispatch, auth]);
-
   return (
     <>
-      <Grid h="100%" templateColumns="repeat(10, 1fr)">
+      <Grid h="100%" templateColumns="repeat(10, 1fr)" >
         <GridItem
+        
           w="100%"
           bg="mainBg.100"
           display={{ base: "none", md: "block" }}
