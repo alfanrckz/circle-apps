ab604da59d4069d375df4b878a9057b56aedee7a
alfanrckz <alfansyuriziaulhaq11@gmail.com>
2024-03-29 09:54:30 +0700

    feat: follow-controller

--- backend/src/controller/FollowController.ts
+++ backend/src/controller/FollowController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import FollowServices from "../services/FollowServices";
+import { boolean } from "joi";
 
 export default new (class FollowController {
   async getFollow(req: Request, res: Response) {
@@ -13,25 +14,26 @@ export default new (class FollowController {
 
   async follow(req: Request, res: Response) {
     try {
-      const response = await FollowServices.follow(
-        req.body.following,
-        res.locals.session.id
-      );
+      const userId = res.locals.session.id;
+      const response = await FollowServices.follow(res, userId, req.body);
       res.status(200).json(response);
     } catch (error) {
-      res.status(500).json({ message: "Internal Server Error" });
+      res.status(500).json({ message: error.message });
     }
   }
 
   async unfollow(req: Request, res: Response) {
     try {
-      const response = await FollowServices.unfollow(
-        req.query.following,
-        res.locals.session.id
-      );
+      // const following = typeof req.query.following === 'string' ? req.query.following : '';
+      const following = req.body.following;
+      console.log("following :", following);
+      
+      const follower = res.locals.session.id.toString();
+      const response = await FollowServices.unfollow(following, follower);
       res.status(200).json(response);
     } catch (error) {
       res.status(500).json({ message: "Internal Server Error" });
     }
   }
+  
 })();
