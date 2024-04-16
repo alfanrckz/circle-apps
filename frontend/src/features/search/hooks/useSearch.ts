b8a3f7d81f2ef365d15e12d8c31ad4b281f52f3f
alfanrckz <alfansyuriziaulhaq11@gmail.com>
2024-04-16 10:08:43 +0700

    feat: use-search

--- frontend/src/features/search/hooks/useSearch.ts
+++ frontend/src/features/search/hooks/useSearch.ts
@@ -4,37 +4,34 @@ import { API } from "../../../libs/api";
 import { GET_USERS } from "../../../stores/rootReducer";
 import { useEffect, useState } from "react";
 
+export async function getUsers(dispatch : any) {
+  try {
+    const response = await API.get("/users");
+    dispatch(GET_USERS(response.data));
+    console.log(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export function useSearch() {
   const dispatch = useDispatch();
   const users = useSelector((state: RootState) => state.user.user);
   const [filteredUsers, setFilteredUsers] = useState(users);
   const profile = useSelector((state: RootState) => state.profile);
-  // console.log(profile);
+
 
   useEffect(() => {
-
-    async function getUsers() {
-      try {
-        const response = await API.get("/users");
-        dispatch(GET_USERS(response.data));
-        // console.log(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getUsers();
-  }, []);
-
-  useEffect(() => {}, []);
+    getUsers(dispatch); 
+  }, [dispatch]);
 
   const searchUsers = (searchQuery: string) => {
     const filtered = users.filter((user) =>
       user.fullName?.toLowerCase().includes(searchQuery.toLowerCase())
     );
     const filteredUserLogin = filtered.filter((user) => user.id !== profile.id);
-    if (filtered) setFilteredUsers( filteredUserLogin);
-
+    if (filtered) setFilteredUsers(filteredUserLogin);
   };
 
-  return { filteredUsers, searchUsers, users,  };
+  return { filteredUsers, searchUsers, users };
 }
