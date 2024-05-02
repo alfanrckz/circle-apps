3d25b3d25cfb783d15a4689e3b327b6bb744edaf
alfanrckz <alfansyuriziaulhaq11@gmail.com>
2024-05-03 02:39:36 +0700

    redux

--- frontend/src/stores/rootReducer.ts
+++ frontend/src/stores/rootReducer.ts
@@ -3,12 +3,12 @@ import { authSlice, threadSlice } from "./slices";
 import { followSlice } from "./slices/followSlice";
 import { userSlice } from "./slices/userSlice";
 import profileSlice from "./slices/profileSlice";
+import profileById from "./slices/profileById";
 
 export const { AUTH_CHECK, AUTH_LOGIN, AUTH_ERROR, AUTH_LOGOUT } =
   authSlice.actions;
 export const { GET_THREADS, SET_THREAD_LIKE } = threadSlice.actions;
-export const { GET_FOLLOW } =
-  followSlice.actions;
+export const { GET_FOLLOW } = followSlice.actions;
 export const { GET_USERS } = userSlice.actions;
 
 export const authReducer = authSlice.reducer;
@@ -17,14 +17,15 @@ export const followReducer = followSlice.reducer;
 export const userReducer = userSlice.reducer;
 
 export const profileReducer = profileSlice;
+export const profileByIdReducer = profileById;
 
 const rootReducer = combineReducers({
   auth: authReducer,
   thread: threadReducer,
   follow: followReducer,
   user: userReducer,
-  profile: profileReducer
-
+  profile: profileReducer,
+  profileId: profileByIdReducer,
 });
 
 export default rootReducer;
