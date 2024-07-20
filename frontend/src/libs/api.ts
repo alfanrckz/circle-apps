7acd195aea48c25b62b0df31fa6b5ae8808fe8a1
alfanrckz <alfansyuriziaulhaq11@gmail.com>
2024-07-21 02:50:44 +0700

    api-baseurl

--- frontend/src/libs/api.ts
+++ frontend/src/libs/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "becircle-production.up.railway.app/api/v1",
+  baseURL: "http://localhost:5000/api/v1",
 });
 
 export function setAuthToken(token: string) {
