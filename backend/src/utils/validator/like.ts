26a2919909caf3adbddd6ab00ba67e6c487967af
alfanrckz <alfansyuriziaulhaq11@gmail.com>
2024-08-05 00:37:08 +0700

    update

--- backend/src/utils/validator/like.ts
+++
@@ -1,6 +0,0 @@
-import * as Joi from "joi";
-
-export const createLikeSchema = Joi.object({
-  user: Joi.number(),
-  thread: Joi.number(),
-});
--- backend/src/utils/validator/reply.ts
+++
@@ -1,8 +0,0 @@
-import * as Joi from "joi";
-
-export const replySchema = Joi.object().keys({
-  content: Joi.string().allow().optional(),
-  image: Joi.string().allow().optional(),
-  user: Joi.number().required(),
-  thread: Joi.number(),
-});
